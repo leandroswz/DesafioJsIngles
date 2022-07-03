f1d6f8b7637afc1919e00da05148627aa9a984b2
Leandro Costa <leandrin.costta@gmail.com>
2022-07-03 16:16:36 -0300

    Desafio JS Ingles

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+let movies = [
+    {
+        Comedy: [
+            {
+                Title: "like white ones",
+                Director: "Keenen Ivory Wayans",
+                Year: 2004
+            }
+        ],
+        Action: [
+            {
+                Title: "explosive charge 3",
+                Director: "Louis Leterrier",
+                Year: 2008 
+            }
+        ],
+        Fantasy: [
+            {
+                Title: "Fantastic Beasts: Dumbledore's Secrets",
+                Director: "David Yates",
+                Year: 2022 
+            }
+        ]
+    }
+]
+
+console.log(movies)
+
+// ====================================
+// ====================================
+
+
+let favoriteMovie = "explosive cargo"
+
+if( favoriteMovie === "explosive cargo"){
+    console.log("my favorite movie is:", favoriteMovie)   
+}else{
+    console.log("not even 5 stars")
+}
